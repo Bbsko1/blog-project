972672bcd6d9e168b99cd197693357df3da9cd98
Bbsko <artmq4@gmail.com>
2023-07-01 22:21:48 +0500

    добавлены правила eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,43 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb",
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
     ],
-    'parser': '@typescript-eslint/parser',
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
+    plugins: [
+        'react',
+        '@typescript-eslint',
     ],
-    "rules": {}
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-indent': [2, 4],
+        indent: [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'linebreak-style': ['error', 'windows'],
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'arrow-body-style': 'off',
+        'react/function-component-definition': 'off',
+        'react/require-default-props': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'no-shadow': 'off',
+        'no-underscore-dangle': 'warn',
+        'import/extensions': 'off',
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
 };
