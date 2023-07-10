048b2d511ee4f114a5d4f2320cc277705bb5c08d
Bbsko <artmq4@gmail.com>
2023-07-10 21:43:37 +0500

    исправлена ошибка с отсутствующим файлом стилей переключением темы

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,6 @@ import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { Button } from 'shared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -13,7 +12,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <Button theme="clear" className={classNames(cls.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
+        <Button theme="clear" className={classNames('', {}, [className])} onClick={toggleTheme}>
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </Button>
     );
