6de01ba550bc812d33dcbf7522c1f9fe86a0771c
Bbsko <artmq4@gmail.com>
2023-07-21 17:52:48 +0500

    добавлены темы для кнопки

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -2,14 +2,26 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ButtonHTMLAttributes, FC } from 'react';
 import cls from './Button.module.scss';
 
+type ButtonSize = 'M' | 'L' | 'XL';
+
+enum ButtonType {
+    M = 'size_m',
+    L = 'size_l',
+    XL = 'size_xl',
+}
+
 export enum ButtonThemes {
     CLEAR = 'clear',
     OUTLINE = 'outline',
+    BACKGROUND = 'background',
+    BACKGROUND_INVERTED = 'backgorundInverted',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     theme?: ButtonThemes;
+    size?: ButtonSize,
+    square?: boolean,
 }
 
 export const Button: FC<ButtonProps> = (props) => {
@@ -17,13 +29,21 @@ export const Button: FC<ButtonProps> = (props) => {
         className,
         children,
         theme,
+        size = 'M',
+        square,
         ...otherProps
     } = props;
 
     return (
         <button
             type="button"
-            className={classNames(cls.Button, {}, [className, cls[theme]])}
+            className={
+                classNames(
+                    cls.Button,
+                    { [cls.square]: square },
+                    [className, cls[theme], cls[ButtonType[size]]],
+                )
+            }
             {...otherProps}
         >
             {children}
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -2,7 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonThemes } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
@@ -23,7 +23,16 @@ export const Sidebar = ({ className }: SidebarProps) => {
             className={classNames(cls.Sidebar, { [cls.collapse]: collapse }, [className])}
             data-testid="sidebar"
         >
-            <Button data-testid="toggle-btn" onClick={toggle}>{t('ButtonSidebar')}</Button>
+            <Button
+                data-testid="toggle-btn"
+                className={cls.collapseBtn}
+                size="XL"
+                square
+                theme={ButtonThemes.BACKGROUND_INVERTED}
+                onClick={toggle}
+            >
+                {collapse ? '>' : '<'}
+            </Button>
 
             <div className={cls.switchers}>
                 <ThemeSwitcher />
