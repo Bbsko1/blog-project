979fb5c81231885e0d63994d6dc9cf4beb2950ba
Bbsko <artmq4@gmail.com>
2023-10-19 20:54:40 +0500

    отключен strict ts config

--- src/entities/Profile/model/services/fetchUserData/fetchUserData.ts
+++ src/entities/Profile/model/services/fetchUserData/fetchUserData.ts
@@ -1,12 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
-import { ThunkExtraArgs } from 'app/providers/StoreProvider/config/StateSchema';
 import { Profile } from '../../types/profile';
 
-export const fetchUserData = createAsyncThunk<Profile, undefined, {extra: ThunkExtraArgs}>(
+export const fetchUserData = createAsyncThunk<Profile, undefined, ThunkConfig<string>>(
     'Profile/fetchUserData',
-    async (_, { extra, rejectWithValue }) => {
-        /* const { rejectWithValue, extra } = thunkApi; */
+    async (_, thunkApi) => {
+        const { rejectWithValue, extra } = thunkApi;
         const { api } = extra;
 
         try {
