aa2be56e642f5d179b46140f338143a362299730
Bbsko <artmq4@gmail.com>
2023-10-24 21:00:36 +0500

    Добавлен api для тестов асинхронных экшенов

--- src/features/Auth/model/services/sendAuthForm.test.ts
+++ src/features/Auth/model/services/sendAuthForm.test.ts
@@ -10,20 +10,21 @@ const mockedAxios = jest.mocked(axios, true);
 describe('sendAuthForm.test', () => {
     test('success login', async () => {
         const userData = { id: 1, username: 'name', password: '123123' };
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userData }));
 
         const asyncThunk = new TestAsyncThunk(sendAuthForm);
+        asyncThunk.api.post.mockReturnValue(Promise.resolve({ data: userData }));
         const result = await asyncThunk.callThunk({ username: '123', password: '12312' });
 
         expect(result.meta.requestStatus).toBe('fulfilled');
         expect(asyncThunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userData));
         expect(asyncThunk.dispatch).toHaveBeenCalledTimes(3);
         expect(result.payload).toEqual(userData);
+        expect(asyncThunk.api.post).toHaveBeenCalled();
     });
 
     test('error login', async () => {
-        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const asyncThunk = new TestAsyncThunk(sendAuthForm);
+        asyncThunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const result = await asyncThunk.callThunk({ username: '123', password: '12312' });
 
         expect(result.meta.requestStatus).toBe('rejected');
